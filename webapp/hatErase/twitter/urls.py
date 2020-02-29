bb253cae89755505f249d14e30ca753d1d4bc8ea
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2020-03-01 01:32:37 +0530

    check

--- webapp/hatErase/twitter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'twitter'
-
-urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<admin_id>[0-9]+)/$', views.detail, name = 'detail')
-]
