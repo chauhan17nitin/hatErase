0679dcb16d66d8a9094015fc92b2a438d598d929
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2020-05-05 18:27:33 +0530

    tfidf vec size clarified

--- api-test-code/credentials.py
+++ api-test-code/credentials.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'F1BCRW0AXUlr0wjLE8L6Znm8a'
+consumer_secret = 'HqRCnviPD8TmI05TSL47CA9eL6niFYtRu35CIP6J2F0fhjL9zz'
+access_token = '1163025465423982592-WrNzWHSUZykiwRmYbexhkUa5BGudYZ'
+access_secret = 'bZ0mWxWgFMClId9OoauDLQgT9IlwojmFS65OuGsy0E9QQ'
